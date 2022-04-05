7727c524239797f1ae50bd1b6888b8df0379c4c1
alex0d <alexey.d707@gmail.com>
2022-04-06 00:28:42 +0300

    Forced light mode and removed ActionBar

--- app/src/main/java/ru/mdev/goculture/MainActivity.java
+++ app/src/main/java/ru/mdev/goculture/MainActivity.java
@@ -1,10 +1,12 @@
 package ru.mdev.goculture;
 
 import android.os.Bundle;
+import android.view.Window;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
