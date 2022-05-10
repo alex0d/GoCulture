89ec074adb6f5efd17678cc6b7284004e1207827
alex0d <alexey.d707@gmail.com>
2022-05-10 03:55:55 +0300

    Real rating (connected the database)

--- app/src/main/java/ru/mdev/goculture/ui/rating/RatingFragment.java
+++ app/src/main/java/ru/mdev/goculture/ui/rating/RatingFragment.java
@@ -1,16 +1,26 @@
 package ru.mdev.goculture.ui.rating;
 
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,127 +29,64 @@ import java.util.Comparator;
 import ru.mdev.goculture.R;
 import ru.mdev.goculture.model.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RatingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RatingFragment extends Fragment {
+
+    private DatabaseReference databaseReference;
 
     private Context context;
     private RatingAdapter ratingAdapter;
-    private String[] usernameUsers = new String[]{"Clouds",
-            "Sun",
-            "Partial clouds",
-            "Snow",
-            "Sleet",
-            "Mist",
-            "Clear",
-            "Rain",
-            "Rain thunder",
-            "Fog",
-            "Partial clouds",
-            "Snow",
-            "Sleet",
-            "Mist",
-            "Clear",
-            "Rain",
-            "Sleet",
-            "Mist",
-            "Clear",
-            "Sleet",
-            "Mist",
-            "Clear",};
-    private int[] scoreUsers = new int[]{123,
-            43,
-            23,
-            54,
-            7685,
-            765,
-            65,
-            98,
-            0,
-            8634,
-            23,
-            54,
-            7685,
-            765,
-            65,
-            98,
-            0,
-            23,
-            54,
-            7685,
-            765,
-            65,
-            98,
-            0,
-            23};
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RatingFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RatingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RatingFragment newInstance(String param1, String param2) {
-        RatingFragment fragment = new RatingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ArrayList<User> users = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        ratingAdapter = new RatingAdapter(users);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+        databaseReference.orderByChild("score").limitToLast(20).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                User user = snapshot.getValue(User.class);
+                users.add(0, user);
+                ratingAdapter.notifyItemInserted(0);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                // TODO: Add an update to the rating table when the data in the database changes
+                Log.d("DatabaseOrder", "onChildChanged: " + previousChildName);
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                Log.d("DatabaseOrder", "onChildRemoved: " + snapshot.getKey());
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Log.d("DatabaseOrder", "onChildMoved: " + previousChildName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("DatabaseOrder", "onCancelled");
+            }
+        });
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        context = inflater.getContext();
         View view = inflater.inflate(R.layout.fragment_rating, container, false);
-        initRecyclerView(view);
-        return view;
-    }
 
-    private void initRecyclerView(View view) {
         RecyclerView recyclerView = view.findViewById(R.id.rating_list);
-        ratingAdapter = new RatingAdapter(generateData());
         recyclerView.setAdapter(ratingAdapter);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(inflater.getContext());
         recyclerView.setLayoutManager(layoutManager);
-    }
 
-    private ArrayList<User> generateData() {
-        ArrayList<User> list = new ArrayList<>();
-        for(int i=0;i<usernameUsers.length;i++) {
-            list.add(new User(usernameUsers[i], scoreUsers[i]));
-        }
-        Collections.sort(list, (user1, user2) -> user2.getScore() - user1.getScore());
-        return list;
+        return view;
     }
 }
