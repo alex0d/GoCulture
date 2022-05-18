8467ee87ef1b7905b749fef06ebe46c8a7f0ef81
Kosty <doro.kon@mail.ru>
2022-05-18 14:25:48 +0300

    New location check

--- app/src/main/java/ru/mdev/goculture/LocationService.java
+++ app/src/main/java/ru/mdev/goculture/LocationService.java
@@ -16,20 +16,37 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.j256.ormlite.stmt.query.In;
 
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import ru.mdev.goculture.model.Sight;
+import ru.mdev.goculture.model.User;
+import ru.mdev.goculture.ui.map.MapFragment;
 import ru.mdev.goculture.ui.map.ProximityIntentReceiver;
 import ru.mdev.goculture.ui.map.SightResponseCallback;
 import ru.mdev.goculture.ui.map.SightsCollector;
 
 public class LocationService extends Service {
+
+    NotificationManager notificationManager;
+
+    private DatabaseReference mDatabaseReference;
 
     private final String TAG = "LOCATION_SERVICE";
 
@@ -59,6 +76,11 @@ public class LocationService extends Service {
 
     @Override
     public void onCreate() {
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference("Users");
 
         final SightResponseCallback sightResponseCallback = () -> sights = sightsCollector.getAll();
         sightsCollector = new SightsCollector(sightResponseCallback);
@@ -96,15 +118,15 @@ public class LocationService extends Service {
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 3, listener);
 
-
-        IntentFilter filter = new IntentFilter(PROXY_ALERT_INTENT);
-        context.registerReceiver(new ProximityIntentReceiver(), filter);
+//        IntentFilter filter = new IntentFilter(PROXY_ALERT_INTENT);
+//        context.registerReceiver(new ProximityIntentReceiver(), filter);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        long INTERVAL = 60000;
-        timer.schedule(timerTask, 0, INTERVAL);
+        long INTERVAL = 6000;
+        int delay = 0;
+        timer.schedule(timerTask, delay, INTERVAL);
         return START_STICKY;
     }
 
@@ -114,7 +136,7 @@ public class LocationService extends Service {
         if (locationManager != null) {
             locationManager.removeUpdates(listener);
         }
-        context.unregisterReceiver(new ProximityIntentReceiver());
+        //context.unregisterReceiver(new ProximityIntentReceiver());
     }
 
     private void checkLocation() {
@@ -124,23 +146,39 @@ public class LocationService extends Service {
         }
 
         Location location = getBestLocation();
-        PendingIntent proximityIntent = PendingIntent.getBroadcast(context, 0, new Intent(PROXY_ALERT_INTENT), PendingIntent.FLAG_IMMUTABLE);
-
         if (location == null || sights == null) {
             return;
         }
 
+//        PendingIntent proximityIntent;
+//
+//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+//            proximityIntent = PendingIntent.getBroadcast(context,
+//                    0,
+//                    new Intent(PROXY_ALERT_INTENT),
+//                    PendingIntent.FLAG_IMMUTABLE);
+//            for (Sight sight : sights) {
+//                if ((Math.abs(Math.abs(sight.getPoint().getLat() - location.getLatitude())) < distance)
+//                        && (Math.abs(sight.getPoint().getLon() - location.getLongitude()) < distance)) {
+//                    locationManager.addProximityAlert(sight.getPoint().getLat(),
+//                            sight.getPoint().getLon(),
+//                            POINT_RADIUS,
+//                            PROXY_ALERT_EXPIRATION,
+//                            proximityIntent
+//                    );
+//                }
+//            }
+//        }
+//        else{
         for (Sight sight : sights) {
-            if ((Math.abs(sight.getPoint().getLat() - location.getLatitude()) < distance)
-                    && (Math.abs(sight.getPoint().getLon() - location.getLongitude()) < distance)) {
-                locationManager.addProximityAlert(sight.getPoint().getLat(),
-                        sight.getPoint().getLon(),
-                        POINT_RADIUS,
-                        PROXY_ALERT_EXPIRATION,
-                        proximityIntent
-                );
+            if(getDistance(location.getLatitude(), sight.getPoint().getLat(), location.getLongitude(), sight.getPoint().getLon())
+                    < POINT_RADIUS){
+                addScoreInDb();
+                sendNotification();
+                Log.i(TAG, "entering if");
             }
         }
+        //}
     }
 
     private Location getBestLocation() {
@@ -160,5 +198,41 @@ public class LocationService extends Service {
             }
         }
         return bestLocation;
+    }
+
+    private double getDistance(double lat1, double lat2, double lon1, double lon2){
+        double R = 6378.137;
+        double PI = 3.1415926535;
+        double dLat = Math.abs(lat2 - lat1) * PI / 180;
+        double dLon = Math.abs(lon2 - lon1) * PI / 180;
+        double a = Math.pow(Math.sin((dLat / 2)), 2) + Math.cos(lat1 *
+                PI / 180) * Math.cos(lat2 * PI / 180) * Math.pow((Math.sin(dLon / 2)), 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c * 1000;
+    }
+
+    private void addScoreInDb(){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(firebaseUser == null){
+            return;
+        }
+
+        DatabaseReference userIdReference = mDatabaseReference.child(firebaseUser.getUid());
+        userIdReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                if( user == null){
+                    return;
+                }
+                mDatabaseReference.child(firebaseUser.getUid()).child("score").setValue(user.getScore() + 1);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
