e462ab0658abd3330d98d5ef8531c4641963fb86
alex0d <alexey.d707@gmail.com>
2022-05-21 18:32:36 +0300

    Added avatars in rating fragment

--- app/src/main/java/ru/mdev/goculture/ui/rating/RatingAdapter.java
+++ app/src/main/java/ru/mdev/goculture/ui/rating/RatingAdapter.java
@@ -11,6 +11,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+import coil.Coil;
+import coil.ImageLoader;
+import coil.request.ImageRequest;
+import coil.transform.CircleCropTransformation;
+import io.getstream.avatarview.AvatarView;
 import ru.mdev.goculture.R;
 import ru.mdev.goculture.model.User;
 
@@ -32,7 +37,18 @@ public class RatingAdapter extends RecyclerView.Adapter<RatingAdapter.ViewRow> {
     @Override
     public void onBindViewHolder(@NonNull ViewRow holder, int position) {
         holder.number.setText(String.valueOf(position + 1));
+
         holder.username.setText(arrayList.get(position).getUsername());
+
+        ImageLoader imageLoader = Coil.imageLoader(holder.itemView.getContext());
+        ImageRequest request = new ImageRequest.Builder(holder.itemView.getContext())
+                .data(arrayList.get(position).getAvatarUrl())
+                .crossfade(true)
+                .transformations(new CircleCropTransformation())
+                .target(holder.avatarView)
+                .build();
+        imageLoader.enqueue(request);
+
         holder.score.setText(String.valueOf(arrayList.get(position).getScore()));
     }
 
@@ -48,12 +64,14 @@ public class RatingAdapter extends RecyclerView.Adapter<RatingAdapter.ViewRow> {
 
         TextView number;
         TextView username;
+        AvatarView avatarView;
         TextView score;
 
         public ViewRow(@NonNull View itemView) {
             super(itemView);
             number = itemView.findViewById(R.id.number);
             username = itemView.findViewById(R.id.username);
+            avatarView = itemView.findViewById(R.id.avatar_view);
             score = itemView.findViewById(R.id.score);
         }
     }
